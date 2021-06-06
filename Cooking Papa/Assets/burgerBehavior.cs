f44fc4c2f9f87edf06ad8714bea2843c368ec15f
Yann Vaillant <yann@vaillant.im>
2021-06-07 00:10:05 +0800

    Init project with more test stuff

---
+++ Cooking Papa/Assets/burgerBehavior.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class burgerBehavior : MonoBehaviour 
+{
+    public Transform burgerPositionObj;
+    public float startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = -1.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("click yo");
+        Instantiate(burgerPositionObj, new Vector3(startPos, 3, -3.7f), new Quaternion(0,0,0, 0));
+        startPos += 1f;
+    }
+}
